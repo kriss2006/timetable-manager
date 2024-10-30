3917d2e39d8d6f70533ba875784aa6af44bebd97
kriss20006 <kristiyan.i.ivanov.2020@elsys-bg.org>
2024-10-30 17:51:03 +0200

    classes pagination and looks ok

--- frontend/pages/admin/classes.vue
+++ frontend/pages/admin/classes.vue
@@ -1,13 +1,19 @@
 <template>
-  <h1>Manage classes</h1>
+  <div class="flex flex-col items-center my-2 gap-4 max-w-1/2">
+    <h1 class="text-2xl font-bold">Manage Classes</h1>
 
-  <div>
     <UInput v-model="q" placeholder="Search" />
 
     <UTable
       :loading="!classes.length"
-      :rows="filteredRows"
+      :rows="computedRows"
       :columns="columns"
+    />
+
+    <UPagination
+      v-model="page"
+      :page-count="itemsPerPage"
+      :total="classes.length"
     />
   </div>
 </template>
@@ -25,14 +31,22 @@ const columns = [
 
 const q = ref('')
 
-const filteredRows = computed(() => {
-  if (!q.value) {
-    return classes.value
+const page = ref(1)
+const itemsPerPage = ref(8)
+
+const computedRows = computed(() => {
+  let rows = classes.value
+
+  if (q.value) {
+    rows.filter((row) => {
+      return String(row.name).toLowerCase().includes(q.value.toLowerCase())
+    })
   }
 
-  return classes.value.filter((classs) => {
-    return String(classs.name).toLowerCase().includes(q.value.toLowerCase())
-  })
+  const start = (page.value - 1) * itemsPerPage.value
+  const end = start + itemsPerPage.value
+
+  return rows.slice(start, end)
 })
 
 const store = useAdminStore()
