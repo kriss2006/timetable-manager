f53f6b0613da25d2ef85877faa330620e5e56469
kriss20006 <kristiyan.i.ivanov.2020@elsys-bg.org>
2024-11-20 15:20:18 +0200

    term select

--- frontend/pages/admin/timetable.vue
+++ frontend/pages/admin/timetable.vue
@@ -1,21 +1,35 @@
 <template>
-  <USelectMenu
-    :loading="classesLoading"
-    searchable
-    v-model="selectedClassId"
-    :options="classes"
-    placeholder="Select a class"
-    value-attribute="id"
-    option-attribute="name"
-    class="w-48"
-  />
-</template>
+  <div class="flex flex-col items-center my-2 gap-4 max-w-1/2">
+    <USelectMenu
+      v-model="selectedTerm"
+      :options="[
+        { value: 1, label: '1' },
+        { value: 2, label: '2' },
+      ]"
+      placeholder="Select a term"
+      class="w-36"
+    />
 
+    <USelectMenu
+      :loading="classesLoading"
+      searchable
+      v-model="selectedClassId"
+      :options="classes"
+      placeholder="Select a class"
+      value-attribute="id"
+      option-attribute="name"
+      class="w-36"
+    />
+
+    <UTable :columns="columns"> </UTable>
+  </div>
+</template>
 <script setup lang="ts">
 definePageMeta({
   middleware: ['admin'],
 })
 
+const selectedTerm = ref<1 | 2>()
 const store = useAdminStore()
 
 const { classesLoading } = storeToRefs(store)
@@ -27,4 +41,27 @@ onMounted(async () => {
 })
 
 const selectedClassId = ref<number>()
+
+const columns = [
+  {
+    key: 'monday',
+    label: 'Monday',
+  },
+  {
+    key: 'tuesday',
+    label: 'Tuesday',
+  },
+  {
+    key: 'wednesday',
+    label: 'Wednesday',
+  },
+  {
+    key: 'thursday',
+    label: 'Thursday',
+  },
+  {
+    key: 'friday',
+    label: 'Friday',
+  },
+]
 </script>
